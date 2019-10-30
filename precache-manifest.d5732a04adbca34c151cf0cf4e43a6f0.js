53413a3bab5d95eb6e5722518cd067373048ee40
Ojehomon1 <periju@web.de>
2019-10-30 11:03:08 +0100

    Updates

---
+++ precache-manifest.d5732a04adbca34c151cf0cf4e43a6f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb5b059c161bf466e377fa2fb04a3a36",
+    "url": "/Hello-React/index.html"
+  },
+  {
+    "revision": "e07541c8996ae9686b4e",
+    "url": "/Hello-React/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e86e5d2e32ef3af72014",
+    "url": "/Hello-React/static/js/2.24b88ce0.chunk.js"
+  },
+  {
+    "revision": "e07541c8996ae9686b4e",
+    "url": "/Hello-React/static/js/main.890e1fc7.chunk.js"
+  },
+  {
+    "revision": "3934539d022e737f9ee5",
+    "url": "/Hello-React/static/js/runtime-main.5f1440da.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Hello-React/static/media/logo.25bf045c.svg"
+  }
+]);
